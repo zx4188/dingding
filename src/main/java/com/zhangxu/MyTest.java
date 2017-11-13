9c302e98139ccd14f352a28df06d4089028c4547
zhangxu <zx4188@163.com>
2017-11-13 14:14:36 +0800

    Initial commit

---
+++ src/main/java/com/zhangxu/MyTest.java
@@ -0,0 +1,12 @@
+package com.zhangxu;
+
+/**
+ * @Author zhangxu
+ * @create in 13:50 2017/11/13
+ * @Description
+ */
+public class MyTest {
+    public static void main(String ars[]){
+        System.out.println("hello maven!!");
+    }
+}
