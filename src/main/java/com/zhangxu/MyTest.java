cd6f7b324aa56bf676b15847cd6d551484d38202
zhangxu <zx4188@163.com>
2017-11-13 14:23:37 +0800

    第二次上传到github

--- src/main/java/com/zhangxu/MyTest.java
+++ src/main/java/com/zhangxu/MyTest.java
@@ -2,11 +2,15 @@ package com.zhangxu;
 
 /**
  * @Author zhangxu
- * @create in 13:50 2017/11/13
+ * @Create in 13:50 2017/11/13
  * @Description
  */
 public class MyTest {
     public static void main(String ars[]){
+        String str="hello github!!";
+
         System.out.println("hello maven!!");
+        System.out.println(str);
+
     }
 }
